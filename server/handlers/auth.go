34cd1d5d751a8b31f032758779bcb70cdf600b95
B0YnextDoor <ppetrov228@bk.ru>
2023-03-23 13:17:52 +0300

    auth ref

--- server/handlers/auth.go
+++ server/handlers/auth.go
@@ -1,15 +1,19 @@
 package handlers
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/GOsling-Inc/GOsling/models"
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 )
 
 func (h *Handler) POST_SignUp(c echo.Context) error {
 	user := models.User{
-		Name: c.FormValue("Name"),
-		Surname: c.FormValue("Surname"),
-		Email: c.FormValue("Emaul"),
+		Name:     c.FormValue("Name"),
+		Surname:  c.FormValue("Surname"),
+		Email:    c.FormValue("Email"),
 		Password: c.FormValue("Password"),
 	}
 	if err := user.Validate(); err != nil {
@@ -20,13 +24,22 @@ func (h *Handler) POST_SignUp(c echo.Context) error {
 		c.JSON(401, err.Error())
 		return err
 	}
-
-	return nil
+	if err := h.service.SignUp(user); err != nil {
+		c.JSON(401, err.Error())
+		return err
+	}
+	token, err := h.CreateJWT(user.ID)
+	if err != nil {
+		c.JSON(401, err.Error())
+		return err
+	}
+	c.SetCookie(h.CreateCookie(token))
+	return c.Redirect(200, "/")
 }
 
 func (h *Handler) POST_SignIn(c echo.Context) error {
 	user := models.User{
-		Email: c.FormValue("Emaul"),
+		Email:    c.FormValue("Email"),
 		Password: c.FormValue("Password"),
 	}
 	if err := user.Validate(); err != nil {
@@ -37,8 +50,42 @@ func (h *Handler) POST_SignIn(c echo.Context) error {
 		c.JSON(401, err.Error())
 		return err
 	}
+	if err := h.service.SignIn(user); err != nil {
+		c.JSON(401, err.Error())
+		return err
+	}
+	token, err := h.CreateJWT(user.ID)
+	if err != nil {
+		c.JSON(401, err.Error())
+		return err
+	}
+	c.SetCookie(h.CreateCookie(token))
+	return c.Redirect(200, "/")
+}
 
-	return nil
+func (h *Handler) CreateCookie(token string) *http.Cookie {
+	cookie := &http.Cookie{}
+	cookie.Name = "Token"
+	cookie.Value = token
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	return cookie
+}
+
+func (h *Handler) CreateJWT(id string) (string, error) {
+	claims := models.JWTClaims{
+		ID: id,
+		StandardClaims: jwt.StandardClaims{
+			Id:        "user_id",
+			ExpiresAt: time.Now().Add(24 * time.Hour).Unix(),
+		},
+	}
+
+	rawToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := rawToken.SignedString([]byte("a"))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
 
 /*
